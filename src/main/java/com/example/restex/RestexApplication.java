7cd437e67a58c8c4df73b1e09d592f29c50af120
Yago Goulu <yago.goulu@gmail.com>
2023-09-20 20:06:53 -0300

    init project

---
+++ src/main/java/com/example/restex/RestexApplication.java
@@ -0,0 +1,13 @@
+package com.example.restex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestexApplication.class, args);
+	}
+
+}
