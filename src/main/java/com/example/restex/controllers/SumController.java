3733f2152b01f60b200fac44be65f7d1d5ad0024
Yago Goulu <yago.goulu@gmail.com>
2023-10-02 01:52:34 -0300

    done rateLimiter

--- src/main/java/com/example/restex/controllers/SumController.java
+++ src/main/java/com/example/restex/controllers/SumController.java
@@ -7,9 +7,14 @@ import com.example.restex.exception.ExternalServiceException;
 import com.example.restex.repositories.HistoryRepository;
 import com.example.restex.repositories.OperationRepository;
 import com.example.restex.services.ExternalMockedService;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,48 +26,60 @@ public class SumController {
   private final ExternalMockedService externalService;
   private final OperationRepository operationRepository;
   private final HistoryRepository historyRepository;
+  private final Bucket bucket;
 
   public SumController(ExternalMockedService externalService, OperationRepository operationRepository, HistoryRepository historyRepository) {
     this.externalService = externalService;
     this.operationRepository = operationRepository;
     this.historyRepository = historyRepository;
+    //configuration for rate limiter, set in 3RPM. Each endpoint will consume 1 token per request, and this will refill in 1 minute.
+    Bandwidth limit = Bandwidth.classic(3, Refill.greedy(3, Duration.ofMinutes(1)));
+    this.bucket = Bucket.builder()
+        .addLimit(limit)
+        .build();
   }
 
   @GetMapping("/sum")
   public ResponseEntity<Double> sum(@RequestParam Double first, @RequestParam Double second) {
+    if (bucket.tryConsume(1)) {
+      Double percentage;
+      Double result;
+      try {
+        percentage = externalService.getPercentageService();
 
-    Double percentage;
-    Double result;
-    try {
-      percentage = externalService.getPercentageService();
+        result = (first + second) * (1 + percentage / 100);
 
-      result = (first + second) * (1 + percentage / 100);
+        Operation newOperation = new Operation(first, second, percentage, result);
+        operationRepository.save(newOperation);
 
-      Operation newOperation = new Operation(first, second, percentage, result);
-      operationRepository.save(newOperation);
+        historyRepository.save(new History(Endpoint.SUM, newOperation.getTimestamp(), newOperation));
+        return ResponseEntity.ok(result);
 
-      historyRepository.save(new History(Endpoint.SUM, newOperation.getTimestamp(), newOperation));
-      return ResponseEntity.ok(result);
+      } catch (ExternalServiceException ex) {
+        Operation lastOperation = operationRepository.findLastHistory();
 
-    } catch (ExternalServiceException ex) {
-      Operation lastOperation = operationRepository.findLastHistory();
+        if (lastOperation == null) {
+          historyRepository.save(new History(Endpoint.SUM, LocalDateTime.now(), ex));
+          throw ex;
+        }
 
-      if (lastOperation == null) {
-        historyRepository.save(new History(Endpoint.SUM, LocalDateTime.now(), ex));
-        throw ex;
+        result = lastOperation.getResult();
+        historyRepository.save(new History(Endpoint.SUM, LocalDateTime.now(), lastOperation));
+        return ResponseEntity.ok(result);
       }
-
-      result = lastOperation.getResult();
-      historyRepository.save(new History(Endpoint.SUM, LocalDateTime.now(), lastOperation));
-      return ResponseEntity.ok(result);
     }
+    return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
   }
 
   @GetMapping("/history")
   public ResponseEntity<Page<History>> history(Pageable pageable) {
-    Page<History> history = historyRepository.findAllWithOperation(pageable);
-    //save history call after seeing the history
-    historyRepository.save(new History(Endpoint.HISTORY, LocalDateTime.now()));
-    return ResponseEntity.ok(history);
+    if (bucket.tryConsume(1)) {
+
+      Page<History> history = historyRepository.findAllWithOperation(pageable);
+      //save history call after seeing the history
+      historyRepository.save(new History(Endpoint.HISTORY, LocalDateTime.now()));
+      return ResponseEntity.ok(history);
+    }
+    return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
   }
 }
